f5550547d1c91a4c88ca56a7b9aa3cb97fddc39d
melchisedec <mwineafrod@gmail.com>
2019-07-02 11:59:54 +0300

    Fix: Add faulty records sql query file on selecting country...subgroup

---
+++ faulty_records.sql
@@ -0,0 +1,7 @@
+SELECT * FROM iita_shadetree.estimate_stderror where 
+attribute_system like '%#%' or attribute_user like '%#%' or subgroup like '%#%'
+or tree_user like '%#%' or tree_system like '%#%';
+
+
+
+#country like '%China%' and region like '%East%';
